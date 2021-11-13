b2711e18cf530162318630106fdf03b06fbfb34b
Hafsa-Begum <hafsagood@gmail.com>
2021-11-13 23:00:32 +0600

    'last-updated'

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -8,13 +7,5 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID,
 
 };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCw5KofR0S2KVvNUqJtbpEDbA46kLIEbpU",
-//     authDomain: "baby-lotion.firebaseapp.com",
-//     projectId: "baby-lotion",
-//     storageBucket: "baby-lotion.appspot.com",
-//     messagingSenderId: "978176589901",
-//     appId: "1:978176589901:web:8c9f0340b29759d6325c04"
-// };
 
 export default firebaseConfig;
